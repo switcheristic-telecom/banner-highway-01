ebb3d819daa2680cca15c9e425d52231e7d8925b
Yufeng Zhao <yufeng-zhao@outlook.com>
2025-10-31 11:25:38 -0400

    init vite

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  root: '.',
+  base: '/',
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './js'),
+    },
+  },
+  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.mp4'],
+});
